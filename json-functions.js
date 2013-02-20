4b041f146620cf3bb0852ab9c073952d623be5d8
Unnx Me <unnx@outlook.com>
2013-02-20 17:13:58 -0500

    stringify, translate, objectify changed

---
+++ json-functions.js
@@ -0,0 +1,66 @@
+//   The stringify function translates an object into a JSON-string, where
+// functions are represented like strings too.
+//
+// example: stringify({a:0,b:function(){return 0;}}); 
+//          returns '{"a":0,"b":"function(){return 0;}"}'
+
+function stringify(c) {
+    var b = {};
+    if(typeof(c)!="object"){return(c.toString());};
+    for(var i in c) {
+        if(typeof(c[i])=="object") b[i]=translate(c[i]);
+        if(typeof(c[i])=="function") {
+            b[i]=c[i].toString();
+        } else {
+            if(typeof(c[i])=="object") {
+                b[i]=translate(c[i]);
+            } else {
+                b[i]=c[i];
+            }
+        };
+    }
+    return(b);
+}
+
+//  The translate function just makes string-representations of object's functions.
+
+function translate(c) {
+    var b = {}
+    for(var i in c) {
+        if(typeof(c[i])=="object") b[i]=translate(c[i]);
+        if(typeof(c[i])=="function") {
+            b[i]=c[i].toString();
+        } else {
+            if(typeof(c[i])=="object") {
+                b[i]=translate(c[i]);
+            } else {
+                b[i]=c[i];
+            }
+        };
+    }
+    return b;
+}
+
+//  The objectify function creates an object from a JSON-string, where strings
+//  like '"a":"function(){return 0;}"' become a real functions.
+//
+// example: objectify('{"a":0,"b":"function(){return 0;}"}'); 
+//          returns an object: {a:0,b:function(){return 0;}}
+
+function objectify(c) {
+    var b = {};
+    for(var i in c) {
+        if(typeof(c[i])=="object"){b[i]=objectify(c[i])} else {
+            if(typeof(c[i])=="string") {
+                if(c[i].indexOf("function")===0){ //TODO
+                    b[i]=Function(c[i].substring(c[i].indexOf("(")+1,c[i].indexOf(")")).split(","),c[i].substring(c[i].indexOf("{")+1,c[i].length-1));
+                } else {
+                    b[i] = c[i];
+                }
+            } else {
+                b[i] = c[i];
+            }
+        }
+    }
+    return b;
+}
